9e641973a2414f716f1c33b789528c37a2511bc2
Daniil Karlov <dskarlov@icloud.com>
2023-07-26 23:20:00 +0300

    linters

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
 from django.contrib.auth.models import Group
 
 from core.models import User
@@ -8,12 +7,12 @@ from core.models import User
 
 @admin.register(User)
 class CustomUser(UserAdmin):
-    readonly_fields = ("last_login", "date_joined")
+    readonly_fields = ('last_login', 'date_joined')
     fieldsets = (
-        (None, {"fields": ("username", "password")}),
-        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
-        ("Permissions", {"fields": ("is_active", "is_staff")}),
-        ("Important dates", {"fields": ("last_login", "date_joined")})
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
 
 
